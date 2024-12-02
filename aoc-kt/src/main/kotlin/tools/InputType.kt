4f9c98c466a2c67860489521438a7bb1cf369d8c
Simon Oyen <simon.oyen@smartsteuer.de>
2024-12-02 16:32:59 +0100

    Renames InputDataReader to TestDataReader

--- aoc-kt/src/main/kotlin/tools/InputType.kt
+++ aoc-kt/src/main/kotlin/tools/InputType.kt
@@ -1,10 +1,10 @@
 package io.github.simonoyen.tools
 
-import tools.InputDataReader
+import tools.TestDataReader
 
 enum class InputType(val fileName: String) {
     PART_A("a_test"),
     PART_B("b_test");
-    
-    fun read(year: Int, day: Int) = InputDataReader.readInput(year, day, fileName)
+
+    fun read(year: Int, day: Int) = TestDataReader.readInput(year, day, fileName)
 }
--- aoc-kt/src/main/kotlin/tools/TestDataReader.kt
+++ aoc-kt/src/main/kotlin/tools/TestDataReader.kt
@@ -1,15 +1,12 @@
 package tools
 
-import io.github.simonoyen.tools.InputType
 import java.io.File
 
-object InputDataReader {
+object TestDataReader {
     fun readInput(year: Int, day: Int, fileName: String) = readFile("$year/$day/$fileName")
-    fun readInput(year: Int, day: Int, type: InputType) = readFile("$year/$day/${type.fileName}")
 
     private fun readFile(fileName: String): String {
         return try {
-            //  {}.javaClass.getResource(fileName)!!.readText()
             File(ClassLoader.getSystemResource(fileName).file).readText()
         } catch (npe: NullPointerException) {
             ""
